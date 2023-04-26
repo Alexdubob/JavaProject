1e534f494db14483ddff87aa3185b759e99d0439
Alexreinh <alexander.reinhardt@gmx.at>
2023-04-26 15:23:07 +0200

    First Commit

--- src/Ausbildungsbeispiele/FreeTimeOrWorkTime.java
+++ src/Ausbildungsbeispiele/FreeTimeOrWorkTime.java
@@ -8,18 +8,19 @@ public class FreeTimeOrWorkTime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
+        //setting worktimes
         LocalTime start = LocalTime.parse("08:30");
         LocalTime lunch = LocalTime.parse("12:00");
         LocalTime afternoon = LocalTime.parse("12:30");
         LocalTime end = LocalTime.parse("15:30");
 
+        //Userinput
         System.out.println("Enter a valid Time");
         String input = sc.nextLine();
         LocalTime time = LocalTime.parse(input);
 
-
-        boolean validWorkTime = (!time.isAfter(start) && !time.isBefore(end) && !time.isBefore(lunch) && !time.isAfter(afternoon));
+        //check if the input is withing the worktime
+        boolean validWorkTime = (time.isAfter(start) && time.isBefore(lunch) || time.isAfter(afternoon) && time.isBefore(end));
         System.out.println(validWorkTime);
 
 
---
+++ src/Ausbildungsbeispiele/OachkatzlSchwoaf.java
@@ -0,0 +1,25 @@
+package Ausbildungsbeispiele;
+
+public class OachkatzlSchwoaf {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[101];
+
+        for (int x = 1; x < numbers.length; x++) {
+            numbers[x] = x;
+            System.out.println(numbers[x]);
+
+            if (numbers[x] % 15 == 0){
+                System.out.println("OachkatzlSchwoaf"+"("+ numbers[x]+")");
+            }
+            else if(numbers[x] % 5 == 0){
+                System.out.println("Schwoaf"+"("+ numbers[x]+")");
+            }
+            else if(numbers[x]% 3 == 0) {
+                System.out.println("Oachkatzl " + "(" + numbers[x] + ")");
+            }
+        }
+
+
+    }
+}
