27d25e5bfc1ccbc1dad4727c2e2ea0cfd1bef28a
Alexreinh <alexander.reinhardt@gmx.at>
2023-05-11 11:24:57 +0200

    First Commit

--- src/Ausbildungsbeispiele/Methoden/DivideAndConquer.java
+++ src/Ausbildungsbeispiele/Methoden/DivideAndConquer.java
@@ -1,16 +1,20 @@
 package Ausbildungsbeispiele.Methoden;
+
 import java.util.Random;
 import java.util.Arrays;
 
 public class DivideAndConquer {
     public static void main(String[] args) {
         Random random = new Random();
+
+
         int[] numbers = new int[21];
         int numbersLength = numbers.length;
 
-        for(int i =0; i < numbers.length; i++){
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = random.nextInt(100);
         }
+
 
         int midNumber = numbersLength / 2;
         int[] leftHalf = new int[midNumber];
@@ -29,5 +33,29 @@ public class DivideAndConquer {
         System.out.println(Arrays.toString(leftHalf));
         System.out.println(Arrays.toString(rightHalf));
 
+
+        int leftMax = numbers[0];
+        for (int i = 0; i < midNumber; i++) {
+            if (numbers[i] > leftMax) leftMax = numbers[i];
+        }
+        System.out.println("The maximum from left side: " + leftMax);
+
+        int rightMax = numbers[0];
+        for (int i = midNumber; i < numbers.length; i++) {
+            if (numbers[i] > rightMax) rightMax = numbers[i];
+        }
+
+        System.out.println("The maximum from the right side: " + rightMax);
+        int max = 0;
+        if (rightMax > leftMax) {
+            max = rightMax;
+        } else {
+            max = leftMax;
+        }
+
+        System.out.println("The total maximum is: " + max);
+
+
     }
 }
+
