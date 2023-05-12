d02109a18caef9ee4cda56ac274f19433218aff5
Alexreinh <alexander.reinhardt@gmx.at>
2023-05-12 12:27:51 +0200

    First Commit

--- src/Ausbildungsbeispiele/Datenstrukturen/EnglishDictionary.java
+++ src/Ausbildungsbeispiele/Datenstrukturen/EnglishDictionary.java
@@ -13,25 +13,54 @@ public class EnglishDictionary {
 
 
         int option = 0;
-        System.out.println("");
-        System.out.println("What would you like to do ?");
-        System.out.println("1. Add a new word into the dictionary.");
-        System.out.println("2. Delete a word from the dictionary.");
-        System.out.println("3. Look for a word in the dictionary.");
-        option = scanner.nextInt();
+        while(option != 4) {
+            System.out.println("");
+            System.out.println("What would you like to do ?");
+            System.out.println("1. Add a new word into the dictionary.");
+            System.out.println("2. Delete a word from the dictionary.");
+            System.out.println("3. Look for a word in the dictionary.");
+            System.out.println("4. Leave the Dictionary.");
+            option = scanner.nextInt();
 
 
-        if (option == 1) {
-            System.out.println("What word would you like to add ?");
+            if (option == 1) {
+                System.out.println("What English word would you like to add ?");
+                String addEnglish = scanner.next();
+                System.out.println("Whats the translation of your word?");
+                String addGerman = scanner.next();
+                dictionary.put(addEnglish, addGerman);
+            }
+            String deleteWord = "";
+            if (option == 2) {
+                    System.out.println("What word would you like to delete from the dictionary? ");
+                deleteWord = scanner.next();
+                dictionary.remove(deleteWord);
+                if (!dictionary.containsKey(deleteWord)) {
+                    System.out.println("there is no such word in the dictionary");
+                }
+            }
+
+            if (option == 3) {
+                System.out.println("What word are you lookig for? ");
+                String getWord = scanner.next();
+
+                String key = null;
+                String valueToFind = getWord;
+                for(Map.Entry<String,String>entry:dictionary.entrySet()){
+                    if(entry.getValue().equals(valueToFind)) {
+                        key = entry.getKey();
+                        System.out.println(key);
+                    }
+                    else {
+                        System.out.println(dictionary.get(getWord));
+                    }
+
+                }
+
+
+            }
+
         }
-        if (option == 2) {
-            System.out.println("What word would you like to delete from the dictionary? ");
-        }
-        if (option == 3) {
-            System.out.println("What word are you lookig for? ");
-        }
-
-
         System.out.println(dictionary);
 
 
---
+++ src/Ausbildungsbeispiele/Datenstrukturen/sortList.java
@@ -0,0 +1,36 @@
+package Ausbildungsbeispiele.Datenstrukturen;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+public class sortList {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        List<Integer>intList = new ArrayList<>();
+        for (int i = 0; i <10;i++) {
+            int j = random.nextInt(20);
+            intList.add(j);
+        }
+        System.out.println("unsorted List: " + intList);
+
+
+        for (int i = 0; i < intList.size(); i++) {
+
+            for (int j = intList.size() - 1; j > i; j--) {
+                if (intList.get(i) > intList.get(j)) {
+
+                    int tmp = intList.get(i);
+              //     intList.set(i) = intList.get(i);
+               //     intList.set(j) = tmp;
+
+                }
+
+            }
+
+        }
+        for (int i: intList) {
+            System.out.println(i);
+        }
+
+    }
+}
