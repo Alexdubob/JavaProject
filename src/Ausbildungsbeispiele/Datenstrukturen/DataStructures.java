9f93f7203643ebbf06f2176e61fbbb651dd5d56a
Alexreinh <alexander.reinhardt@gmx.at>
2023-05-12 08:24:15 +0200

    First Commit

---
+++ src/Ausbildungsbeispiele/Datenstrukturen/DataStructures.java
@@ -0,0 +1,7 @@
+package Ausbildungsbeispiele.Datenstrukturen;
+
+public class DataStructures {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Ausbildungsbeispiele/Methoden/DivideAndConquerTest.java
@@ -0,0 +1,43 @@
+package Ausbildungsbeispiele.Methoden;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class DivideAndConquerTest {
+       static Random random = new Random();
+        public static int findMax(int[] arr){
+
+            return findMaxHelper(arr, 0, arr.length - 1);
+        }
+
+        private static int findMaxHelper(int[] arr, int left, int right) {
+            if (left == right) {
+                return arr[left];
+            }
+            else{
+
+                int mid = (left + right) / 2;
+                int maxLeft = findMaxHelper(arr, left, mid);
+                int maxRight = findMaxHelper(arr, mid + 1, right);
+                if (maxLeft > maxRight) {
+                    return maxLeft;
+                } else {
+                    return maxRight;
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            int[] arr = new int[20];
+            for(int i =0; i < arr.length; i++)
+                arr[i] = random.nextInt(100);
+
+            int max = findMax(arr);
+            System.out.println(Arrays.toString(arr));
+            System.out.println("Maximum: " + max);
+
+        }
+    }
+
+
+
