b2e1273b76864da0dfa42c04b44ce67678364353
Alexreinh <alexander.reinhardt@gmx.at>
2023-05-22 14:43:30 +0200

    First Commit

--- src/Ausbildungsbeispiele/OOP1/PeopleManagement.java
+++ src/Ausbildungsbeispiele/OOP1/PeopleManagement.java
@@ -1,4 +1,8 @@
 package Ausbildungsbeispiele.OOP1;
 
+
 public class PeopleManagement {
+    public static void main(String[] args) {
+
+    }
 }
--- src/Ausbildungsbeispiele/Sortieralgorithmen/BingoCardGenerator.java
+++ src/Ausbildungsbeispiele/Sortieralgorithmen/BingoCardGenerator.java
@@ -10,62 +10,19 @@ public class BingoCardGenerator {
         // Fülle die B-Spalte mit 5 zufälligen Nummern aus dem Bereich 1-15
         Set<Integer> usedNumbers = new HashSet<>();
         Random random = new Random();
+
         for (int i = 0; i < 5; i++) {
-            int number;
-            do {
-                number = random.nextInt(15) + 1;
-            } while (usedNumbers.contains(number));
-            usedNumbers.add(number);
-            card[i][0] = number;
+            for (int j = 0; j < 5; j++) {
+                int number;
+                do {
+                    number = random.nextInt(15) + 1 + 15  * j;
+                } while (usedNumbers.contains(number));
+                usedNumbers.add(number);
+                card[i][j] = number;
+            }
         }
         card[2][2] = 0;
 
-        // Fülle die I-Spalte mit 5 zufälligen Nummern aus dem Bereich 16-30
-        usedNumbers.clear();
-        for (int i = 0; i < 5; i++) {
-            int number;
-            do {
-                number = random.nextInt(15) + 16;
-            } while (usedNumbers.contains(number));
-            usedNumbers.add(number);
-            card[i][1] = number;
-        }
-
-        // Fülle die N-Spalte mit 4 zufälligen Nummern aus dem Bereich 31-45
-        usedNumbers.clear();
-        for (int i = 0; i < 4; i++) {
-            int number;
-            do {
-                number = random.nextInt(15) + 31;
-            } while (usedNumbers.contains(number));
-            usedNumbers.add(number);
-
-            card[i][2] = number;
-
-
-        }
-
-        // Fülle die G-Spalte mit 5 zufälligen Nummern aus dem Bereich 46-60
-        usedNumbers.clear();
-        for (int i = 0; i < 5; i++) {
-            int number;
-            do {
-                number = random.nextInt(15) + 46;
-            } while (usedNumbers.contains(number));
-            usedNumbers.add(number);
-            card[i][3] = number;
-        }
-
-        // Fülle die O-Spalte mit 5 zufälligen Nummern aus dem Bereich 61-75
-        usedNumbers.clear();
-        for (int i = 0; i < 5; i++) {
-            int number;
-            do {
-                number = random.nextInt(15) + 61;
-            } while (usedNumbers.contains(number));
-            usedNumbers.add(number);
-            card[i][4] = number;
-        }
 
         // Gib die Bingo-Karte formatiert auf der Konsole aus
         System.out.println("B  I  N  G  O");
