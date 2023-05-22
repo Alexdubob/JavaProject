a58fb7d63eed700e5394dd5220cd04d02970b843
Alexreinh <alexander.reinhardt@gmx.at>
2023-05-22 15:22:13 +0200

    First Commit

--- src/Ausbildungsbeispiele/OOP1/PeopleManagement.java
+++ src/Ausbildungsbeispiele/OOP1/PeopleManagement.java
@@ -2,7 +2,28 @@ package Ausbildungsbeispiele.OOP1;
 
 
 public class PeopleManagement {
+
+    public void createperson(String vorname, String nachname){
+        Person person = new Person(vorname,nachname);
+        personenListe.add(person);
+    }public void createperson(String vorname, String nachname, String geburtsdatum, String adresse, String geschlecht){
+        Person person = new Person(vorname,nachname, geburtsdatum, adresse,geschlecht);
+        personenListe.add(person);
+    }public void createperson(String vorname, String nachname,String geburtstadum){
+        Person person = new Person(vorname,nachname, geburtstadum);
+        personenListe.add(person);
+    }
+    public void deletePerson(Person person){
+        personenListe.remove(person);
+    }
     public static void main(String[] args) {
+        PeopleManagement pv= new PeopleManagement();
+        pv.createperson("John","Doe");
+        pv.createperson("Jane","Smith","01.01.1990",""
+        pv.createperson();
 
     }
+
+
+
 }
