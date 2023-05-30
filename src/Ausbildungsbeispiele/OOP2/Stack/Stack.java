98b371956764d4bd3afb12fae39b64577c3e819f
Alexreinh <alexander.reinhardt@gmx.at>
2023-05-30 15:24:55 +0200

    First Commit

--- src/Ausbildungsbeispiele/OOP2/Stack/Stack.java
+++ src/Ausbildungsbeispiele/OOP2/Stack/Stack.java
@@ -1,18 +1,33 @@
 package Ausbildungsbeispiele.OOP2.Stack;
-
 public class Stack {
+    Node head;
+    class Node {
+        int value;
+        Node next;
+        Node(int d) {
+            value = d;
+            next = null;
+        }
+    }
     public static void main(String[] args) {
         Stack stack = new Stack();
 
     }
     void push (int newElement){
-        for
+
 
     }
     public int size(){
-
+        Node node = head;
+        int size = 0;
+        while (node != null) {
+            size++;
+            node = node.next;
+        }
+        return size;
     }
-    public int pop(){
+
+   /* public int pop(){
 
     }
     public int peek(){
@@ -20,5 +35,5 @@ public class Stack {
     }
     public int[] pop(int n){
 
-    }
+    }*/
 }
