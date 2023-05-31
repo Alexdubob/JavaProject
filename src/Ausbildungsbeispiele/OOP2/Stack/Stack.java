ab74d70645769346a31779363a1e00187305ee47
Alexreinh <alexander.reinhardt@gmx.at>
2023-05-31 10:59:10 +0200

    First Commit

--- src/Ausbildungsbeispiele/OOP2/Stack/Stack.java
+++ src/Ausbildungsbeispiele/OOP2/Stack/Stack.java
@@ -1,23 +1,50 @@
 package Ausbildungsbeispiele.OOP2.Stack;
 public class Stack {
     Node head;
-    class Node {
+
+    static class Node {
         int value;
         Node next;
+
         Node(int d) {
             value = d;
             next = null;
         }
     }
+
     public static void main(String[] args) {
         Stack stack = new Stack();
+        Stack stack2 = new Stack ();
 
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60);
+        stack.push(70);
+        stack.push(80);
+        stack.push(90);
+        stack.pop();
+        stack.printList();
+        System.out.println("\npeek = " + stack.peek());
+        System.out.println("size = " + stack.size());
+        //stack.pop();
+        stack2 = stack.pop(2);
+        stack2.printList();
+        System.out.println();
+        stack.printList();
     }
-    void push (int newElement){
 
-
+    //inserts a new element at the top of the stack
+    public void push(int newElement) {
+        Node newNode = new Node(newElement);
+        newNode.next = head;
+        head = newNode;
     }
-    public int size(){
+
+    //returns the number of elements in the stack
+    public int size() {
         Node node = head;
         int size = 0;
         while (node != null) {
@@ -27,13 +54,45 @@ public class Stack {
         return size;
     }
 
-   /* public int pop(){
-
+    //returns the last element of the stack and removes it from the stack
+    public void pop(){
+        if (head == null)
+            return;
+        head = head.next;
     }
-    public int peek(){
+    public void deleteNode(Node del) {
+        System.out.println(head);
+        if (head == null)
+            return;
 
+        if (head == del)
+            head = del.next;
     }
-    public int[] pop(int n){
 
-    }*/
+    //returns the last element of the stack without modifying the stack
+   public int peek(){
+       if (head == null) {
+           System.out.println("Double linked List is empty");
+       }
+       return head.value;
+    }
+
+    //returns the last n elements of the stack and removes them from the stack
+    public Stack pop (int n) {
+        Stack s = new Stack();
+        for (int i = 0; i < n; i++) {
+            s.push(head.value);
+            head = head.next;
+        }
+        return s;
+    }
+    //prints the whole stack
+    public void printList() {
+        Node n = head;
+        while (n != null) {
+            System.out.print(n.value + " ");
+            n = n.next;
+        }
+    }
 }
+
